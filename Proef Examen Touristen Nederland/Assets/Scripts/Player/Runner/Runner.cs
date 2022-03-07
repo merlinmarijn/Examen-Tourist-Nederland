0799a952ecd79529320049b0fedcbce981789ff8
merlinmarijn <ratched10@hotmail.nl>
2022-03-07 11:24:23 +0100

    New Minigame Scene / logic
    -New scene (runner minigame)
    -New runner player

--- Proef Examen Touristen Nederland/Assets/Scripts/Player/Runner/Runner.cs
+++ Proef Examen Touristen Nederland/Assets/Scripts/Player/Runner/Runner.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Runner : MonoBehaviour
 {
+    public CharacterController controller;
+    public Transform cam;
+
+    public float speed = 6f;
+
+    public float turnSmoothTime = 0.1f;
+    float _turnSmoothVelocity;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = 1;
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
     }
 }
